15411d46022c4658e124a6c135ede9bd702e8813
aad60601 <82053003+aad60601@users.noreply.github.com>
2022-01-17 13:20:31 +0500

    Create 03_burger.py

---
+++ lab3/part1/03_burger.py
@@ -0,0 +1,45 @@
+import my_burger
+my_burger.ing1(2)
+my_burger.ing5(2)
+my_burger.ing2(1)
+my_burger.ing6(2)
+my_burger.ing2(1)
+my_burger.ing6(2)
+my_burger.ing4(2)
+my_burger.ing3(4)
+print('Ваш двойной чизбургер, готов!\n')
+# Создать рецепт своего бургера, по вашему вкусу.
+# Если не хватает ингридиентов - создать соответствующие функции в модуле my_burger
+my_burger.ing1(2)
+my_burger.ing8(2)
+my_burger.ing5(2)
+my_burger.ing6(2)
+my_burger.ing2(1)
+my_burger.ing7(2)
+my_burger.ing4(2)
+my_burger.ing3(4)
+print('Рецепт моего любимого бургера!')
+
+
+my_burger.py
+def ing1(bun):
+    print (f'А теперь добавим:{bun} булочки')
+
+def ing2(cutlets):
+    print (f'А теперь добавим:{cutlets} котлету')
+
+def ing3(cucumbers):
+    print (f'А теперь добавим:{cucumbers} огурчика')
+
+def ing4(tomato):
+    print (f'А теперь добавим:{tomato} помидорки')
+
+def ing5(mayonnaise):
+    print (f'А теперь добавим:{mayonnaise} ложки майонеза')
+
+def ing6(cheese):
+    print (f'А теперь добавим:{cheese} ломтика сыра')
+def ing7(becon):
+    print (f'А теперь добавим:{becon} ломтика бекона')
+def ing8(ketchup):
+    print (f'А теперь добавим:{ketchup} ложки кетчупа')
